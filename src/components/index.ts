1a7d2f017b088d11c561c6270b06880e65168b09
unknown <krzys.repsch@gmail.com>
2023-05-06 19:43:22 +0200

    feat: add missing components exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
 export { default as Cursor } from './Cursor';
 export { default as NavLinks } from './NavLinks';
+export { default as PersonalInfoList } from './PersonalInfoList';
