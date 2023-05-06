3cefff0a0922d79becba1e328786d75f9a702c82
unknown <krzys.repsch@gmail.com>
2023-05-06 22:52:05 +0200

    feat: add OrderedList component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export { default as ContactForm } from './ContactForm';
 export { default as Cursor } from './Cursor';
 export { default as FormField } from './FormField';
 export { default as NavLinks } from './NavLinks';
+export { default as OrderedList } from './OrderedList';
 export { default as PersonalInfoList } from './PersonalInfoList';
