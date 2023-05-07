9e7b8def6bbade824cbbd68cd441f869de7b044d
unknown <krzys.repsch@gmail.com>
2023-05-07 21:52:24 +0200

    feat: update aboutMe content

--- src/config/aboutMe.tsx
+++ src/config/aboutMe.tsx
@@ -6,61 +6,55 @@ already reading this line I'm pretty sure that you
 became interested in my portfolio. So without
 unnecessary dummy talking I'd rather provide you with
 something about me, unless you want only a bare facts.
-Then just click my resume. Social cream is going to be
-on the next button.`;
+Then just click "My resume". Social cream is going to be
+under "Read More" button.`;
 
 export const moreAboutMe = (
 	<>
 		<section>
 			<h2>Who am I exactly?</h2>
 			<p className="is-style-text">
-				My name is Chris, and I currently live in Otwock. I'm 22 and
-				some people think that I'm a looser without a proper education.
-				Despite this I don't think like that about myself and the proof
-				of that is a place where I am currently in my life. By that I
-				mean that I'm searching for my first job as a Junior Frontend
-				Developer. When I'm into something I'm always get like 200% of
-				my involvement which can be proven by my really polished
-				projects that you can find later in my portfolio. I perceive my
-				current activities as kind of investment in my future career. I
-				go to work everyday but after that always try to spend my time
-				to take out as many of my planned tasks as I can. I always plan
-				my schedule up to 2 or 3 days ahead. Am I a boring person then?
-				It's not for me to judge, though my interests are neither fancy
-				nor sublime. Apart from programming I've got a romance with
-				classical cinematography and rock music. I can also debate about
-				dogs behaviorism since my parents have a breeding so I've
-				learned a lot of things till now.
+				I'm Chris, currently living in Otwock. I'm 24 years old and I
+				have over a year of professional experience as a Frontend
+				Developer. I believe the words "He's a man of focus, commitment,
+				and sheer will" by John Wick describe me the best. When I take
+				on something, my dedication is limitless, as evidenced by my
+				polished projects that you can find on my GitHub. I consider the
+				present as an investment in the future. My work doesn't end at 4
+				p.m. cause there's always something worth learning! Am I a
+				boring person then? It's possible, it's not for me to judge,
+				although I don't have particularly refined interests. Apart from
+				programming, I can include a love for movies and rock music
+				among them. I can also talk about dog behavior due to my parents
+				running a Maltese dog breeding business, so I've picked up some
+				knowledge in the meantime. You'll find more details in my CV.
 			</p>
 		</section>
 		<section>
 			<h2>How did I learned these things?</h2> <br />
 			<p className="is-style-text">
-				My journey with programming started about two years ago. One day
-				I've accidentally came across a vlog on Youtube with big banner
-				which said „You don't need math to become a programmer” and it
-				wasn't a video of some company that will make you a programmer
-				in just 3 months! After 3 months I found out that making notes
-				(writing whole code in my notebook) is a little bit pointless.
-				Back to the question above I am self-taught person and all of my
-				skills are from courses, documentation but mostly from practice.
-				To be honest my learning path probably looked similar like
-				others. HTML, CSS → JavaScript → Framework (and tons of more or
-				less useful things in between)
+				I started my programming journey a little over 3 years ago. One
+				day, I stumbled upon a vlog with a catchy title that said, "You
+				don't need to know math to become a programmer." And no, it
+				wasn't a video from one of those companies that promises to turn
+				you into a programmer in 3 months. After 3 months, I realized
+				that taking notes in a notebook (i.e., transcribing code) was
+				pointless. I am a self-taught programmer, and everything I know
+				I owe to online courses, documentation, and above all, practice.
 			</p>
 		</section>
 		<section>
 			<h2>Why should you hire me?</h2>
 			<p className="is-style-text">
-				My dear recruiter, if you have read all of my scribble I would
-				like to thank you for your time! To be honest I don't wanna
-				answer that question because I'm pretty sure that you know
-				routine formula like „I am gonna be faithful and loyal
-				programmer and promise to do my best”. Despite this I would say
-				that I'm a human being who just wanna reset his life in some
-				way, start everything from scratch in a new and friendly company
-				by doing what I love the most. The Rest is on your hands so as
-				they say „With great power comes great responsibility!”
+				I don't wanna answer the question above in a typical formulaic
+				manner, such as saying that I'll be a loyal and dedicated
+				programmer who will give my best. You probably hear that from
+				everyone. Instead, I'll boldly say that I'm good at programming,
+				and this is just the beginning of my career, so I haven't fully
+				taken off yet. It's all in your hands, "With great power comes
+				great responsibility!" and so on. Dear recruiter, if you've made
+				it to the end of my ramblings, I want to sincerely thank you for
+				your time (don't scan that QR code at the end of my portfolio).
 			</p>
 		</section>
 	</>
