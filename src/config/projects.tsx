cc105c2a7e6f328ba78e39d0ae850331473f45fa
unknown <krzys.repsch@gmail.com>
2023-07-02 00:08:08 +0200

    feat: activate projects section & add projects dummy data

---
+++ src/config/projects.tsx
@@ -0,0 +1,44 @@
+const projects = [
+	{
+		title: 'La Mia Passione Maltese',
+		description: 'sdaadsdas',
+		features: [
+			'Toggle between different years in "Exhibitions section"',
+			'Toggle answers in "Faq section"',
+			'Email sending (emailjs) with form validation (react-hook-form)',
+			'Popover usage (popperjs)',
+			'Cool animations (framer-motion)',
+		],
+		reposytoryUrl:
+			'https://github.com/BlackAndWhiteCarnage/La-Mia-Passione-Maltese-V2',
+		usedTools: ['react'],
+		image: {
+			src: 'https://images.unsplash.com/photo-1601645191163-3fc0d5d64e35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+			alt: '',
+		},
+	},
+	{
+		title: 'My Charming Dolls',
+		description: 'sdaadsdas',
+		features: [
+			'Payments stripe',
+			'Marking products as sold after successful payment',
+			'Strapi Backend',
+			'Images are hosted on Cloudinary',
+			'Slider swiper',
+			'Sending email with emailjs',
+			'Form validation with react-hook-form',
+			'Stories storybook',
+			'All "readable" data from Strapi is in markdown format react-markdown',
+		],
+		reposytoryUrl:
+			'https://github.com/BlackAndWhiteCarnage/La-Mia-Passione-Maltese-V2',
+		usedTools: ['Next.js'],
+		image: {
+			src: 'https://images.unsplash.com/photo-1601645191163-3fc0d5d64e35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+			alt: '',
+		},
+	},
+];
+
+export default projects;
--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,59 +1,69 @@
 /**
  * External dependencies
  */
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 /**
  * Internal dependencies
  */
-import { Poster, Slider } from '@/components';
+import { Popup, Poster, ProjectDescription, Slider } from '@/components';
+import { projects } from '@/config';
+import { ReactComponent as HeroLandscape } from '@/images/hero-about-me-landscape.svg';
+import { ReactComponent as HeroPortrait } from '@/images/hero-about-me-portrait.svg';
+import { usePopup } from '@/hooks';
 import classes from './Projects.module.scss';
 
-const Projects: FC = () => (
-	<div className={classes.wrapper} id="projects">
-		<span className={classes.info}>
-			Please be informed that I have recently been fully engaged in
-			professional commitments, which have limited my time for personal
-			projects. Consequently, I believe that the code I previously
-			authored may not accurately reflect my current skill set. However, I
-			am excited to announce that I'm currently in the process of
-			enhancing my previous projects. They will be published soon,
-			showcasing my current capabilities. In the meantime, I invite you to
-			visit my{' '}
-			<a
-				className={classes.link}
-				href="https://github.com/BlackAndWhiteCarnage"
-				target="_blank"
-				rel="noreferrer"
-			>
-				GitHub profile
-			</a>{' '}
-			where you can explore my past and upcoming projects.
-		</span>
-		<Slider>
-			<Poster
-				image={{
-					src: 'https://images.unsplash.com/photo-1601645191163-3fc0d5d64e35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-					alt: '',
-				}}
-				onClick={() => {}}
-			/>
-			<Poster
-				image={{
-					src: 'https://images.unsplash.com/photo-1601645191163-3fc0d5d64e35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-					alt: '',
-				}}
-				onClick={() => {}}
-			/>
-			<Poster
-				image={{
-					src: 'https://images.unsplash.com/photo-1601645191163-3fc0d5d64e35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-					alt: '',
-				}}
-				onClick={() => {}}
-			/>
-		</Slider>
-	</div>
-);
+const Projects: FC = () => {
+	const [activeProject, setActiveProject] = useState('');
+
+	const popup = usePopup();
+
+	return (
+		<>
+			<div className={classes.wrapper} id="projects">
+				<Slider>
+					{projects.map(({ title, image }, index) => (
+						<Poster
+							image={image}
+							key={index}
+							onClick={() => setActiveProject(title)}
+						/>
+					))}
+				</Slider>
+			</div>
+			{projects.map(
+				(
+					{
+						title,
+						description,
+						features,
+						image,
+						reposytoryUrl,
+						usedTools,
+					},
+					index
+				) => (
+					<Popup
+						{...popup}
+						key={index}
+						landscapeBackround={HeroLandscape}
+						portraitBackround={HeroPortrait}
+						title={title}
+						isOpen={activeProject === title}
+						close={() => setActiveProject('')}
+					>
+						<ProjectDescription
+							description={description}
+							features={features}
+							image={image}
+							reposytoryUrl={reposytoryUrl}
+							usedTools={usedTools}
+						/>
+					</Popup>
+				)
+			)}
+		</>
+	);
+};
 
 export default Projects;
