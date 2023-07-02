f1113946c33447c1aab87fd2d31d7d8841ad84c9
unknown <krzys.repsch@gmail.com>
2023-07-02 13:14:16 +0200

    feat: update my resume

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -21,43 +21,34 @@ const cx = classnames.bind(classes);
 
 const Slider: FC<SliderProps> = ({ children, className, ...props }) => (
 	<div className={cx('wrapper', className)}>
-		<Atropos
-			rotateTouch="scroll-y"
-			rotateYMax={6}
-			rotateXMax={6}
-			shadow={false}
-			highlight={false}
-			className={classes.innerWrap}
+		<Swiper
+			modules={[Pagination]}
+			className={classes.swiperWrapper}
+			slidesPerView="auto"
+			centeredSlides
+			pagination={{
+				clickable: true,
+			}}
+			initialSlide={1}
+			slideToClickedSlide
+			{...props}
 		>
-			<Swiper
-				modules={[Pagination]}
-				className={classes.swiperWrapper}
-				slidesPerView="auto"
-				centeredSlides
-				pagination={{
-					clickable: true,
-				}}
-				initialSlide={1}
-				slideToClickedSlide
-				{...props}
-			>
-				{children.map((item, index) => (
-					<SwiperSlide key={index} className={classes.slideWrapper}>
-						{({ isActive, isNext, isPrev }) => (
-							<div
-								className={cx('slide', {
-									isActive,
-									isNext,
-									isPrev,
-								})}
-							>
-								{item}
-							</div>
-						)}
-					</SwiperSlide>
-				))}
-			</Swiper>
-		</Atropos>
+			{children.map((item, index) => (
+				<SwiperSlide key={index} className={classes.slideWrapper}>
+					{({ isActive, isNext, isPrev }) => (
+						<div
+							className={cx('slide', {
+								isActive,
+								isNext,
+								isPrev,
+							})}
+						>
+							{item}
+						</div>
+					)}
+				</SwiperSlide>
+			))}
+		</Swiper>
 	</div>
 );
 
